03e1a46cb40e9951ec78aba59a0df6efc057b34b
hibeeeekay <60797412+hibeeeekay@users.noreply.github.com>
2021-02-22 16:13:01 -0800

    Add files via upload

---
+++ COOKIE&SESSION/answer.php
@@ -0,0 +1,3 @@
+<?php
+  echo "The difference between cookie and session variable is that,the data stored in a session variable is destroyed when the session ends while the data stored in cookies will stay alive as they are stored on the local machine so the web browser can access them every time a user log in to a website. "
+?>
